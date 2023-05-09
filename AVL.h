9bf08f9a5f6e0c126681d1e8b9b4b85dda58c31b
Sama Ahmed <102187124+SamaAhmedS@users.noreply.github.com>
2023-05-09 22:50:52 +0300

    Update AVL.h

--- AVL.h
+++ AVL.h
@@ -7,27 +7,26 @@
 #include<map>
 using namespace std;
 
-template<class T>
 class AVL {
 
 public:
-	Node<T>* root;
+	Node* root;
 	AVL();
-	Node<T>* createNode(student s);
-	Node<T>* rotateleft(Node<T>* root);
-	Node<T>* rotateright(Node<T>* root);
-	int height(Node<T>* root);
-	Node<T>* add_student(Node<T>* root, student s);
-	Node<T>* delete_student(Node<T>* root, student s);
-	Node<T>* findmin(Node<T>* root);
-	bool search_student(Node<T>* root, int id);
+	Node* createNode(student s);
+	Node* rotateleft(Node* root);
+	Node* rotateright(Node* root);
+	int height(Node* root);
+	Node* add_student(Node* root, student s);
+	Node* delete_student(Node* root, student s);
+	Node* findmin(Node* root);
+	bool search_student(Node* root, int id);
 	void insert();
 	void delete_std(int id);
 	void search(int id);
-	void printReq(Node<T>* cur);
+	void printReq(Node* cur);
 	void printInOrder();
 	bool validData(string& n, string& dep, int& id, double& gpa);
-	Node<T>* balanceBST(Node<T>* root);
+	Node* balanceBST(Node* root);
 	int start();
 	map<string, int> depMap;
 
