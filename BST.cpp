f6ebc200b8d058defe311ac3043fc95c6b61df83
Sama Ahmed <102187124+SamaAhmedS@users.noreply.github.com>
2023-05-09 21:30:40 +0300

    Update BST.cpp

--- BST.cpp
+++ BST.cpp
@@ -1,12 +1,57 @@
 #include"BST.h"
-template<class T>
-void BST<T>::insert(T val) {
-	Node<T>* addedStudent = new Node<T>(val);
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+int BST::start() {
+	int option;
+	cout << "Choose one of the following options:\n1. Add student\n2. Remove student\n3. Search student\n"
+		<< "4. Print All(sorted by id)\n5. Return to main menu\n";
+	cin >> option;
+	while (option < 1 || option>5) {
+		cout << "Invalid choice.\n Please choose one of the following options:\n1. Add student\n2. Remove student\n3. Search student\n"
+			<< "4. Print All(sorted by id)\n5. Return to main menu\n"<<"Enter number of option:";
+		cin >> option;
+	}
+	switch (option)
+	{
+	case 1: {
+		AddStudent();
+		break;
+	}
+	case 2: {
+		cout << "your choice 2\n";
+		break;
+	}
+	case 3: {
+		int id;
+		cout << "id:";
+		cin >> id;
+		SearchStudent(id);
+		break;
+	}
+	case 4: {
+		PrintAll();
+		break;
+	}
+	case 5: {
+		cout << "your choice 5\n";
+
+		option = 0;
+		break;
+	}
+
+	default:
+		break;
+	}
+	return option;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void BST::insert(student val) {
+	Node* addedStudent = new Node(val);
 	if (root == nullptr) {
 		root = addedStudent;
 		return;
 	}
-	Node<T>* cur = root;
+	Node* cur = root;
 	while (cur != nullptr) {
 		if (cur->value < addedStudent->value) {//insert in right
 			if (cur->right == nullptr) {
@@ -25,64 +70,35 @@ void BST<T>::insert(T val) {
 	}
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-template<class T>
-void printReq(Node<T>* cur) {
+int departments[4];
+void printReq(Node* cur) {
 	if (cur == nullptr) {
 		return;
 	}
-	Node<T>* r = cur;
-
 	printReq(cur->left);
 	cout << cur->value << "\n";
+	string dep = cur->value.getDepart();
+	if (dep == "CS") {
+		departments[0]++;
+	}
+	else if (dep == "IT") {
+		departments[1]++;
+	}
+	else if (dep == "DS") {
+		departments[2]++;
+	}
+	else {
+		departments[3]++;
+	}
 	printReq(cur->right);
 }
-template<class T>
-void BST<T>::printInOrder() {
+void BST::PrintAll() {
+	
 	printReq(root);
+	cout << "CS " << departments[0] << " Students\nIT " << departments[1] << " Students\n";
+	cout << "DS " << departments[2] << " Students\nIS " << departments[3] << " Students\n";
 }
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-int BSTStudent::start() {
-	int option;
-	cout << "Choose one of the following options:\n1. Add student\n2. Remove student\n3. Search student\n"
-		<< "4. Print All(sorted by id)\n5. Return to main menu\n";
-	cin >> option;
-	while (option < 1 || option>5) {
-		cout << "Invalid choice.\n Please choose one of the following options:\n1. Add student\n2. Remove student\n3. Search student\n"
-			<< "4. Print All(sorted by id)\n5. Return to main menu\n" << "Enter number of option:";
-		cin >> option;
-	}
-	switch (option)
-	{
-	case 1: {
-		AddStudent();
-		break;
-	}
-	case 2: {
-		cout << "your choice 2\n";
-		break;
-	}
-	case 3: {
-		cout << "your choice 3\n";
-		break;
-	}
-	case 4: {
-		//PrintAll();
-		break;
-	}
-	case 5: {
-		cout << "your choice 5\n";
-
-		option = 0;
-		break;
-	}
-
-	default:
-		break;
-	}
-	return option;
-}
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 bool validData(string& n, string& dep, int& id, double& gpa) {
 
 	regex validN("[a-zA-Z]+ [a-zA-Z]+"), validD("[A-Z][A-Z]");
@@ -100,9 +116,9 @@ bool validData(string& n, string& dep, int& id, double& gpa) {
 	//	return true;
 	//}
 	//return false;
-	return true;
+    return true;
 }
-void BSTStudent::AddStudent() {
+void BST::AddStudent() {
 	string n, dep;
 	double gpa; int id;
 	bool valid = 0;
@@ -117,14 +133,26 @@ void BSTStudent::AddStudent() {
 		std::getline(std::cin, dep);
 		valid = validData(n, dep, id, gpa);
 	}
-	students.insert(student(n, dep, id, gpa));
+	insert(student(n, dep, id, gpa));
 	cout << "The student is added.\n";
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-void BSTStudent::PrintAll() {
-	students.printInOrder();
+void BST::SearchStudent(int id) {
+	if (id < 0) {
+		cout << "Invalid ID must be positive integer\n";
+	}
+	Node* cur = root;
+	while (cur != nullptr) {
+		if (cur->value.getID() == id) {
+			cout << "Student is found.\n" << cur->value << "\n";
+			return;
+		}
+		else if (cur->value.getID() < id) {
+			cur = cur->right;
+		}
+		else if (cur->value.getID() > id) {
+			cur = cur->left;
+		}
+	}
+	cout << "Student is found.\n";
 }
-
-
-
